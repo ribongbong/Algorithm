9f44abffee2e0aa5596a12069a1d739445e7ba9d
ribongbong <ribongbong3@gmail.com>
2023-03-09 10:14:09 +0900

    test

--- programmers1.js
+++ programmers1.js
@@ -2,8 +2,8 @@ function solution(nums) {
     let answer = 0;
     let count = nums.length / 2;
     
-    let arr = nums.filter((element, index) => {
-        return nums.indexOf(element) === index;
+    let arr = nums.filter((element, idx) => {
+        return nums.indexOf(element) === idx;
     });
     
     if(arr.length >= count){
